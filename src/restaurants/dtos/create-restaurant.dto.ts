7454c11f028cfd2bfdd1fe76d3e26dc4bdf0d8bd
Noah <noahshin11@gmail.com>
2022-01-27 23:35:47 +0900

    [Add] chapter 2 is done

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,16 +1,20 @@
 import { ArgsType, Field, InputType } from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from "class-validator";
 
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(type => String)
+  @Length(5,10)
+  @IsString()
   name: string
+  @IsBoolean()
   @Field(type => Boolean)
   isVegan?: boolean
-
+  @IsString()
   @Field(type => String)
   address: string
-
+  @IsString()
   @Field(type => String)
   ownersName: string
 }
